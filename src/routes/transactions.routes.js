1abb31940b84d024d45dc21f3af434bdd65bb361
Realex78 <36749154+Realex78@users.noreply.github.com>
2026-04-26 23:29:48 -0600

    Register SSE route

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -3,10 +3,12 @@ const {
   getTransactions,
   getTransaction,
 } = require("../controllers/transactions.controller");
+const { streamTransactions } = require("../controllers/sse.controller");
 
 const router = express.Router();
 
 router.get("/", getTransactions);
+router.get("/stream", streamTransactions);
 router.get("/:id", getTransaction);
 
 module.exports = router;
